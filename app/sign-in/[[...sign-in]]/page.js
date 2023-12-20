a0a83b42ab6df0947b0262bfc3fa9b046b7113d1
sanujasethi06 <sethisanuja1998@gmail.com>
2023-12-20 23:25:37 +0530

    login and signup form added

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,20 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <div>
+        <Image
+          src="/uber-logo.jpg"
+          width={900}
+          height={1000}
+          className="object-contain w-full h-full"
+        />
+        <div className="absolute top-10 right-0">
+          <SignIn />
+        </div>
+      </div>
+    </>
+  );
 }
--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,19 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
+
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div>
+      <Image
+        src="/uber-logo.jpg"
+        width={900}
+        height={900}
+        className="object-contain w-full h-full"
+      />
+      <div className="absolute top-10 right-0">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
