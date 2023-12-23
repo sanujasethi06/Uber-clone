466c4e77a8780ce3272ce45d77d437e4d3da9f5f
sanujasethi06 <sethisanuja1998@gmail.com>
2023-12-23 12:53:57 +0530

    fetching geolocation from the api

--- app/componenets/Home/InputItem.js
+++ app/componenets/Home/InputItem.js
@@ -1,6 +1,30 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-const InputItem = ({type}) => {
+
+
+const InputItem = ({ type }) => {
+    
+    const [value, setValue] = useState(null);
+    const [placeholder, setPlaceholder] = useState(null);
+    
+    useEffect(() => {
+        type == 'source' ? setPlaceholder('Pickup Loacation') : setPlaceholder('Dropoff Location');
+    }, []);
+
+    const getLatLng = (place, type) => {
+        console.log(place);
+        const placeId = place.value.place_id;
+        const service = new google.maps.places.PlacesService(document.createElement('div'));
+        service.getDetails({ placeId }, (place, status) => {
+          if (status == "OK" && place.geometry && place.geometry.location) {
+              console.log(place.geometry.location.lat());
+              console.log(place.geometry.location.lng());
+          }
+        });
+    };
+    
     return (
       <>
         <div className="bg-slate-200 p-3 rounde-lg mt-3 flex items-center gap-2">
@@ -39,7 +63,7 @@ const InputItem = ({type}) => {
               </svg>
             )}
           </div>
-          <div>
+          {/* <div>
             <input
               type="text"
               placeholder={
@@ -47,10 +71,28 @@ const InputItem = ({type}) => {
               }
               className="bg-transparent w-full outline-none"
             />
+        </div> */}
+          <GooglePlacesAutocomplete
+            apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+            selectProps={{
+              value,
+                onChange: (place) => {
+                    getLatLng(place, type);
+                    setValue(place);
+                },
+                placeholder: placeholder,
+                isClearable: true,
+                className: "w-full",
+                components: { DropdownIndicator: false },
+                styles: {
+                    control: (provided) => ({
+                        ...provided,
+                        backgroundColor: '#00ffff00',
+                    border: 'none'})
+              }
+            }}
+          />
         </div>
-          </div>
-             
-        
       </>
     );
 }
