a1851789324952206c335f21c7693cd6b3d71496
sanujasethi06 <sethisanuja1998@gmail.com>
2023-12-26 16:52:08 +0530

    added custom google map

--- app/componenets/Home/GoogleMapSection.js
+++ app/componenets/Home/GoogleMapSection.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const GoogleMapSection = () => {
-  return (
-    <div>
-      Google Map Section
-    </div>
-  )
+
+  const containerStyle = {
+    width: "100%",
+    height:window.innerWidth*0.45
+  };
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      options={{ mapId: "311400266b9695aa" }}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
 export default GoogleMapSection
