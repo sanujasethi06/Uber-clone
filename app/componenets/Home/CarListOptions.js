cd970d24f6a2337550923cda57cb5aea3ee9c0e4
sanujasethi06 <sethisanuja1998@gmail.com>
2024-01-02 15:23:18 +0530

    added dynamic make payment method

--- app/componenets/Home/CarListOptions.js
+++ app/componenets/Home/CarListOptions.js
@@ -5,23 +5,26 @@ import { CarListsData } from '@/app/utils/CarListsData'
 const CarListOptions = (  {distance}) => {
 
   const [activeIndex, setActiveIndex] = useState();
+  const [selectedCar,setSelectedCar] = useState();
   return (
     <div className="mt-5 p-5 overflow-auto h-[300px] ">
       <h2 className="text-[22px] font-bold">Recommended</h2>
       {CarListsData.map((item, index) => (
         <div
-          className={`cursor-pointer p-2 px-4 rounded-md border-black ${
-            activeIndex == index ? "border-[3px] " : null
-          }`}
-          onClick={() => setActiveIndex(index)}
+          className={`cursor-pointer p-2 px-4 rounded-md border-black ${activeIndex == index ? "border-[3px] " : null
+            }`}
+          onClick={() => {
+            setActiveIndex(index);
+            setSelectedCar(item)
+          }}
         >
           <CarListItem car={item} distance={distance} />
         </div>
       ))}
-      <div className='flex justify-between fixed bottom-5 bg-white p-3 shadow-xl w-full md:w-[30%] border-[1px] items-center rounded-lg'>
+      {selectedCar?.name?<div className='flex justify-between fixed bottom-5 bg-white p-3 shadow-xl w-full md:w-[30%] border-[1px] items-center rounded-lg'>
         <h2 className="">Make Payment For</h2>
-        <button className="p-3 bg-black text-white rounded-lg text-center">Request UberX</button>
-      </div>
+        <button className="p-3 bg-black text-white rounded-lg text-center">Request { selectedCar.name}</button>
+      </div>:null}
     </div>
   );
 }
